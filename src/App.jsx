17d9a20307552662f1209d469b8bd2195466ed4e
Lukas Stöver <lukas.stoever@googlemail.com>
2026-03-03 12:37:49 +0100

    Update homepage via Telegram

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>ONS Gallery Berlin 2026</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
